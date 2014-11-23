d2516df07d11a5927a86c45293a759f5585ef560
Brandon Henry <brandon.m.henry@gmail.com>
2014-11-22 20:49:15 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject scoreboard "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Event driven OM football scoreboard."
+  :url "https://evening-taiga-7916.herokuapp.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :source-paths ["src/clj" "src/cljs"]
-
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
@@ -19,14 +17,10 @@
                  [com.cemerick/piggieback "0.1.3"]
                  [weasel "0.4.0-SNAPSHOT"]
                  [leiningen "2.5.0"]]
-
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
-
   :min-lein-version "2.5.0"
-
   :uberjar-name "scoreboard.jar"
-
   :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
@@ -35,20 +29,14 @@
                                         :externs       ["react/externs/react.js"]
                                         :optimizations :none
                                         :pretty-print  true}}}}
-
   :profiles {:dev {:repl-options {:init-ns scoreboard.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
                    :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
-
                    :figwheel {:http-server-root "public"
                               :port 3449
                               :css-dirs ["resources/public/css"]}
-
                    :env {:is-dev true}
-
                    :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
-
              :uberjar {:hooks [leiningen.cljsbuild]
                        :env {:production true}
                        :omit-source true
