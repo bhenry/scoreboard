27d0e5f3421ead259c57dcd8348fef521dda197c
Brandon Henry <brandon.m.henry@gmail.com>
2014-11-22 19:59:36 -0500

    better update number handling

--- src/cljs/scoreboard/core.cljs
+++ src/cljs/scoreboard/core.cljs
@@ -3,25 +3,84 @@
             [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]))
 
-(reset! app-state
-        {:text "ScoreBoard+"
-         :home {:home? true
-                :score 0
-                :timeouts 3}
-         :away {:home? false
-                :score 0
-                :timeouts 3}
-         :ball-on 50
-         :to-go 10
-         :down 1
-         :qtr 1
-         :time "15:00"
-         :posession :home})
+(defonce init
+  (reset! app-state
+          {:text "ScoreBoard+"
+           :home {:home? true
+                  :score 0
+                  :timeouts 3}
+           :away {:home? false
+                  :score 0
+                  :timeouts 3}
+           :ball-on 50
+           :to-go 10
+           :down 1
+           :qtr 1
+           :time "15:00"
+           :posession :home}))
 
 (defn publish-event! [t & [ev]]
   (fn [e]
     (.preventDefault e)
     (e/publish! (e/event t ev))))
+
+(defn number-editor [cursor owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:editing? false})
+    om/IDidUpdate
+    (did-update [_ prev-props prev-state]
+      (let [input (. js/document
+                     (getElementById (:dom-id cursor)))]
+        (when input
+          (.focus input))))
+    om/IRenderState
+    (render-state [_ {:keys [editing?]}]
+      (let [event-info (select-keys cursor [:path :max :min])
+            {:keys [dom-id dom-label display-fn number]} cursor
+            action-fn (fn [e]
+                        (.preventDefault e)
+                        (e/publish!
+                         (e/event ::update-number
+                                  (merge event-info
+                                         {:number (.-value
+                                                   (. js/document
+                                                      (getElementById dom-id)))
+                                          :editor owner}))))]
+        (html
+         [:div {:class dom-id}
+          (when dom-label
+            [:div.click {:on-click (publish-event! ::inc-number event-info)
+                         :on-context-menu (publish-event! ::dec-number event-info)}
+             dom-label])
+          (if editing?
+            [:div
+             [:form {:on-submit action-fn}
+              [:input {:id dom-id
+                       :type "text"
+                       :size 3
+                       :on-blur action-fn}]]]
+            [:div.click {:on-click #(do (.preventDefault %)
+                                        (om/set-state! owner :editing? true))}
+             (if display-fn (display-fn number) number)])])))))
+
+(defn ball-on-editor [ball-on]
+  (om/build number-editor ball-on
+            {:fn (fn [ball-on]
+                   {:path [:ball-on]
+                    :number ball-on
+                    :dom-id "ball-on"
+                    :dom-label "ball on"
+                    :max 50
+                    :display-fn #(if (zero? %) "G" %)})}))
+
+(defn score-editor [score home?]
+  (om/build number-editor score
+            {:fn (fn [n]
+                   {:path [(if home? :home :away) :score]
+                    :number n
+                    :dom-id (if home? "homescore" "awayscore")})}))
 
 (defn team [team owner]
   (reify
@@ -30,50 +89,12 @@
       (let [{:keys [home? score timeouts posession]} team]
         (html
          [:div.team
-          [:div.score.click
-           {:on-click (publish-event! ::inc-score
-                                      {:home? home?})
-            :on-context-menu (publish-event! ::dec-score
-                                             {:home? home?})}
-           score]
+          (score-editor score home?)
           [:div.timeouts.click
            {:on-click (publish-event! ::dec-timeout
                                       {:home? home?})}
            timeouts]
           [:div.posession (if posession "X" "_")]])))))
-
-(defn ball-on-editor [ball-on owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:editing? false})
-    om/IDidUpdate
-    (did-update [_ prev-props prev-state]
-      (let [ball-on-input (. js/document
-                             (getElementById "ball-on"))]
-        (if ball-on-input
-          (.focus ball-on-input))))
-    om/IRenderState
-    (render-state [_ {:keys [editing?]}]
-      (html
-       [:div.ball-on.left
-        [:div "ball on"]
-        (if editing?
-          [:div
-           [:form
-            [:input#ball-on {:type "text" :size 3}]
-            [:br]
-            [:input {:type "submit"
-                     :value "change"
-                     :on-click #(do (.preventDefault %)
-                                    (e/publish!
-                                     (e/event ::change-ball-on
-                                              {:ball-on (.-value (. js/document
-                                                                    (getElementById "ball-on")))
-                                               :editor owner})))}]]]
-          [:div.click {:on-click (publish-event! ::edit-ball-on
-                                           {:editor owner})}
-           ball-on])]))))
 
 (defn scoreboard [app owner]
   (reify
@@ -89,8 +110,12 @@
                             :posession (= :home (:posession app)))})]
          [:div.keys
           [:div.teams
-           [:div.left "Home"]
-           [:div.right "Away"]]
+           [:div.left {:on-click (publish-event! ::inc-score {:home? true})
+                       :on-context-menu (publish-event! ::dec-score {:home? true})}
+            "Home"]
+           [:div.right {:on-click (publish-event! ::inc-score {:home? false})
+                        :on-context-menu (publish-event! ::dec-score {:home? false})}
+            "Away"]]
           [:div.timeouts "TIMEOUTS"]
           [:div.posession.click
            {:on-click (fn [e]
@@ -111,9 +136,8 @@
           [:div "to go"]
           [:div.click {:on-click (publish-event! ::inc-to-go)
                  :on-context-menu (publish-event! ::dec-to-go)}
-           (:to-go app)]]
-         (om/build ball-on-editor
-                   (:ball-on app))
+           (if (zero? (:to-go app)) "in." (:to-go app))]]
+         [:div.left (ball-on-editor (:ball-on app))]
          [:div.quarter.left
           [:div "qtr"]
           [:div.click {:on-click (publish-event! ::inc-qtr)}
@@ -125,15 +149,18 @@
    app-state
    {:target (. js/document (getElementById "app"))}))
 
+(defn dec-0 [n]
+  (max 0 (dec n)))
+
 (defn inc-score [{:keys [home?]}]
   (swap! app-state update-in [(if home? :home :away) :score] inc))
 
 (defn dec-score [{:keys [home?]}]
-  (swap! app-state update-in [(if home? :home :away) :score] dec))
+  (swap! app-state update-in [(if home? :home :away) :score] dec-0))
 
 (defn dec-timeout [{:keys [home?]}]
   (swap! app-state update-in [(if home? :home :away) :timeouts]
-         #(if (zero? %) 3 (dec %))))
+         #(if (zero? %) 3 (dec-0 %))))
 
 (defn change-posession [_]
   (swap! app-state update-in [:posession] #(if (= :home %) :away :home)))
@@ -150,13 +177,22 @@
   (swap! app-state update-in [:to-go] inc))
 
 (defn dec-to-go [_]
-  (swap! app-state update-in [:to-go] dec))
+  (swap! app-state update-in [:to-go] dec-0))
 
-(defn edit-ball-on [{:keys [editor]}]
+(defn edit-number [{:keys [editor]}]
   (om/set-state! editor [:editing?] true))
 
-(defn change-ball-on [{:keys [ball-on editor]}]
-  (swap! app-state update-in [:ball-on] (fn [_] ball-on))
+(defn- between [upper lower n]
+  (max (or lower 0) (min (or upper 10000) n)))
+
+(defn inc-number [{:keys [path max min]}]
+  (swap! app-state update-in path #(between max min (inc (int %)))))
+
+(defn dec-number [{:keys [path max min]}]
+  (swap! app-state update-in path #(between max min (dec (int %)))))
+
+(defn update-number [{:keys [path number max min editor]}]
+  (swap! app-state update-in path (fn [_] (between max min (int number))))
   (om/set-state! editor [:editing?] false))
 
 (defonce subscriptions
@@ -169,5 +205,7 @@
    [::inc-down] inc-down
    [::inc-to-go] inc-to-go
    [::dec-to-go] dec-to-go
-   [::edit-ball-on] edit-ball-on
-   [::change-ball-on] change-ball-on))
+   [::edit-number] edit-number
+   [::inc-number] inc-number
+   [::dec-number] dec-number
+   [::update-number] update-number))
